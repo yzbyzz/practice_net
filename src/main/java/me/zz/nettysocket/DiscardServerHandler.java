71446598211c2ed6ecaa3162cdcc3985dad3ddff
yzbyzz <zhezheng.z@foxmail.com>
2022-11-01 08:24:54 +0800

    feat: 增加 Netty Discard Server
    https://netty.io/wiki/user-guide-for-4.x.html

---
+++ src/main/java/me/zz/nettysocket/DiscardServerHandler.java
@@ -0,0 +1,27 @@
+package me.zz.nettysocket;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import me.zz.common.Logger;
+import me.zz.common.LoggerFactory;
+
+/**
+ * @author zz
+ * @date 2022/11/01
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+    private static final Logger log = LoggerFactory.getLogger(DiscardServerHandler.class);
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        log.debug("read, msg:[%s]", msg);
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
--- src/main/java/me/zz/nettysocket/NettySocketServer.java
+++ src/main/java/me/zz/nettysocket/NettySocketServer.java
@@ -1,8 +1,54 @@
 package me.zz.nettysocket;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 /**
  * @author zz
  * @date 2022/10/29
  */
 public class NettySocketServer {
+
+    private int port;
+
+    public NettySocketServer(int port) {
+        this.port = port;
+    }
+
+    public void run() {
+
+        EventLoopGroup bossEventLoopGroup = new NioEventLoopGroup();
+        EventLoopGroup workerEventLoopGroup = new NioEventLoopGroup();
+
+        try {
+
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossEventLoopGroup, workerEventLoopGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer() {
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+                            ch.pipeline().addLast(new DiscardServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+            ;
+
+            ChannelFuture future = bootstrap.bind(port).sync();
+            future.channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossEventLoopGroup.shutdownGracefully();
+            workerEventLoopGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new NettySocketServer(7070).run();
+    }
 }
